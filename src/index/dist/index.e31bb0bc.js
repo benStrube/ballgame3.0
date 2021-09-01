bf0c742908684a9980381a4534c59ad0eab949f8
ben.strube <benjamin.strube@ust.com>
2021-09-01 16:34:56 -0400

    creating player paddle

--- src/index/dist/index.e31bb0bc.js
+++ src/index/dist/index.e31bb0bc.js
@@ -117,13 +117,59 @@ parcelRequire = (function (modules, cache, entry, globalName) {
   }
 
   return newRequire;
-})({"index.js":[function(require,module,exports) {
+})({"../game/paddle.js":[function(require,module,exports) {
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = void 0;
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
+
+var Paddle = /*#__PURE__*/function () {
+  function Paddle(gameHeight, GameWidth) {
+    _classCallCheck(this, Paddle);
+
+    this.width = 150;
+    this.height = 30;
+    this.position = {
+      x: GameWidth / 2 - this.width / 2,
+      y: gameHeight - this.height - 10
+    };
+  }
+
+  _createClass(Paddle, [{
+    key: "draw",
+    value: function draw(ctx) {
+      ctx.fillStyle = '#00f';
+      ctx.fillRect(this.position.x, this.position.y, this.width, this.height);
+    }
+  }]);
+
+  return Paddle;
+}();
+
+exports.default = Paddle;
+},{}],"index.js":[function(require,module,exports) {
+"use strict";
+
+var _paddle = _interopRequireDefault(require("./../game/paddle"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
 var canvas = document.getElementById("gameScreen");
 var ctx = canvas.getContext('2d');
+var GAME_HEIGHT = 600;
+var GAME_WIDTH = 800;
 ctx.clearRect(0, 0, 800, 600);
-ctx.fillStyle = '#f00';
-ctx.fillRect(20, 20, 100, 100);
-},{}],"../../../../../Users/bstrube/AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+var paddle = new _paddle.default(GAME_HEIGHT, GAME_WIDTH);
+paddle.draw(ctx);
+},{"./../game/paddle":"../game/paddle.js"}],"../../../../../Users/bstrube/AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
--- src/index/index.js
+++ src/index/index.js
@@ -1,7 +1,13 @@
+import Paddle from './../game/paddle';
+
 let canvas = document.getElementById("gameScreen");
 let ctx = canvas.getContext('2d');
 
+const GAME_HEIGHT = 600;
+const GAME_WIDTH = 800;
+
 ctx.clearRect(0,0,800,600);
 
-ctx.fillStyle = '#f00';
-ctx.fillRect(20,20,100,100);
+let paddle = new Paddle(GAME_HEIGHT,GAME_WIDTH);
+
+paddle.draw(ctx);
